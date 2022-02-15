e47a45c534c43b715243069818d7a18a2a57b652
Patrick Latorre <ptrckltrr@gmail.com>
2022-02-15 16:55:44 +0800

    Implement cache

--- node/dao/Cache.ts
+++ node/dao/Cache.ts
@@ -1,4 +1,5 @@
 import { AbstractIterator } from "abstract-leveldown";
+import { rejects } from "assert";
 import EncodingDown from "encoding-down"
 import { LevelUp } from "levelup";
 import MemDown from "memdown";
@@ -10,18 +11,39 @@ import { IMetadataStore } from "./IMetadataStore";
 class FileSysCache implements IMetadataStore {
 
   /* In-memory db containing all IServer data. */
-  db = LevelUp(
-    EncodingDown<string, IServer>(MemDown(), { valueEncoding: 'json' })
-  );
+  // db = LevelUp(
+  //   EncodingDown<string, IServer>(MemDown(), { valueEncoding: 'json' })
+  // );
+
+  private javaMap       = new Map<string, string>();
+  private templateMap   = new Map<string, string>();
+  private serverMap     = new Map<string, IServer>();
 
 
-  listJavaNames(): Promise<string[]> {
+  async listJavaNames(): Promise<string[]> {
     throw new Error("Method not implemented.");
   }
   
   
   listServerIds(): Promise<string[]> {
-    throw new Error("Method not implemented.");
+    return new Promise((resolve, reject) => {
+      const ids = [...this.serverMap.keys()];
+      resolve(ids);
+    });
+  }
+
+
+  listServers(): Promise<IServer[]> {
+    return new Promise((resolve, reject) => {
+      const servers: IServer[] = [];
+
+      for (const [srvId, srv] of Object.entries(this.serverMap)) {
+        srv.ini.id = srvId;
+        servers.push(srv);
+      }
+
+      resolve(servers);
+    });
   }
   
   
@@ -31,32 +53,55 @@ class FileSysCache implements IMetadataStore {
   
   
   getServerMetadata(srvId: string): Promise<IServer> {
-    throw new Error("Method not implemented.");
+    return new Promise((resolve, reject) => {
+      const srv = this.serverMap.get(srvId);
+      
+      if (srv !== undefined) {
+        srv.ini.id = srvId;
+        resolve(srv);
+      } else {
+        reject(`Server ${srvId} doesn't exist.`);
+      }
+    });
   }
   
   
   saveServerMetadata(srvId: string, srv: IServer): Promise<void> {
-    throw new Error("Method not implemented.");
+    return new Promise((resolve, reject) => {
+      srv.ini.id = srvId;
+      this.serverMap.set(srvId, srv);
+      resolve();
+    });
   }
   
   
-  getIni(srvId: string): Promise<IServerIni> {
-    throw new Error("Method not implemented.");
+  async getIni(srvId: string): Promise<IServerIni> {
+    const srv = await this.getServerMetadata(srvId);
+    srv.ini.id = srvId;
+    return srv.ini;
   }
   
   
-  saveIni(srvId: string, ini: IServerIni): Promise<void> {
-    throw new Error("Method not implemented.");
+  async saveIni(srvId: string, ini: IServerIni): Promise<void> {
+    const srv = await this.getServerMetadata(srvId);
+    srv.ini = ini;
+    srv.ini.id = srvId;
+    await this.saveServerMetadata(srvId, srv);
   }
   
   
-  getProps(srvId: string): Promise<IServerProps> {
-    throw new Error("Method not implemented.");
+  async getProps(srvId: string): Promise<IServerProps> {
+    const srv = await this.getServerMetadata(srvId);
+    srv.ini.id = srvId;
+    return srv.props;
   }
   
   
-  saveProps(srvId: string, props: IServerProps): Promise<void> {
-    throw new Error("Method not implemented.");
+  async saveProps(srvId: string, props: IServerProps): Promise<void> {
+    const srv = await this.getServerMetadata(srvId);
+    srv.props = props;
+    srv.ini.id = srvId;
+    await this.saveServerMetadata(srvId, srv);
   }
 
 }
